658e391050734c9b35b97e4c5660fd36105f90af
Dumitru Panaghiea <panaghiea@gmail.com>
2017-01-04 11:16:31 +0000

    Correctly indent the files with spaces

--- specs.js
+++ specs.js
@@ -4,12 +4,12 @@ chai.should();
 describe('Example of tests that run automatically when specs.js is modified', () => {
     describe('This suite will demonstrate the way mocha displays passing and failing tests', () => {
         it('This test should pass', () => {
-			let str = "1";			
-			str.should.be.a('string');
+            let str = "1";
+            str.should.be.a('string');
         });
         it('This test should faill', () => {
-			let int = "0";			
-			int.should.be.a('number');
+            let int = "0";
+            int.should.be.a('number');
         });
     });
 });
