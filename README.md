e71b54ecd9c08ef61fbf8eb2a4f3d82069cb748a
Dumitru Panaghiea <panaghiea@gmail.com>
2017-01-04 09:43:07 +0000

    Add skeleton; add tests; update readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # browser-tests-skeleton
 A simple skeleton for running tests in the browser
+
+**Setup**
+
+    npm install
+    
+**Run**
+
+    npm start
+
+This command should open the default browser pointing to http://127.0.0.1:8080/. 
+
+Currently there are two tests running. For demonstration purposes one is passing and one is failing. 
+
+**Notes**
+
+Runs with Node 4+. 
---
+++ specs.js
@@ -0,0 +1,15 @@
+var should = chai.should;
+chai.should();
+
+describe('Example of tests that run automatically when specs.js is modified', () => {
+    describe('This suite will demonstrate the way mocha displays passing and failing tests', () => {
+        it('This test should pass', () => {
+			let str = "1";			
+			str.should.be.a('string');
+        });
+        it('This test should faill', () => {
+			let int = "0";			
+			int.should.be.a('number');
+        });
+    });
+});
